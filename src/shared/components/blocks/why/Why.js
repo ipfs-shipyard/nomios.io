c75f48160a87a9e83a131642fdab82ba9d0409fc
Nelson Fonseca <nelsonfonseca.18@gmail.com>
2019-06-03 16:30:36 +0100

    feat: add layout constrains

--- src/shared/components/blocks/why/Why.js
+++ src/shared/components/blocks/why/Why.js
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './Why.module.css';
+import LayoutContainer from '../../../components/layout-container/LayoutContainer';
 
 const Why = ({ className }) => (
-    <div className={ classNames(styles.why, className) }>
-        This is the why!
-    </div>
+    <LayoutContainer>
+        <div className={ classNames(styles.why, className) }>
+            This is the why!
+        </div>
+    </LayoutContainer>
 );
 
 Why.propTypes = {
---
+++ src/shared/components/layout-container/LayoutContainer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+// Styles
+import styles from './LayoutContainer.module.css';
+
+class LayoutContainer extends Component {
+    render() {
+        const { children, className, contentClassName, ...props } = this.props;
+
+        return (
+            <section className={ styles.container } { ...props }>
+                <div className={ classNames(contentClassName, styles.content) }>
+                    { children }
+                </div>
+            </section>
+        );
+    }
+}
+
+LayoutContainer.propTypes = {
+    className: PropTypes.string,
+    contentClassName: PropTypes.string,
+    children: PropTypes.node.isRequired,
+};
+
+export default LayoutContainer;
