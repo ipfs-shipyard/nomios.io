7f8c48ce5c099ae937a877352d69604acaf9738f
Ricardo <ricardos8977@gmail.com>
2019-06-03 15:52:25 +0100

    feat: add github and workshop blocks
    Closes #158

--- src/shared/components/blocks/check-github/CheckGithub.js
+++ src/shared/components/blocks/check-github/CheckGithub.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './CheckGithub.module.css';
 import Button from '../../button';
+import { GithubIcon } from '../../icon';
+import { isBelowSmallDesktop } from '../../../utils/windowInfo';
 
-const CheckGithub = ({ className }) => (
-    <div className={ classNames(styles.checkGithub, className) }>
-        This is the check github!
-        <Button variant="primary" >PRIMARY</Button>
-        <Button variant="secondary" >SECONDARY</Button>
-        <Button variant="tertiary" >TERTIARY</Button>
-    </div>
-);
+class CheckGithub extends Component {
+    state = {
+        width: 'none',
+    };
+
+    componentDidMount() {
+        this.updateDimensions();
+        window.addEventListener('resize', this.updateDimensions.bind(this));
+    }
+
+    render() {
+        const { className } = this.props;
+
+        return (
+            <div className={ classNames(styles.checkGithub, className) }>
+                <div className={ styles.content }>
+                    <div className={ styles.title }>{ isBelowSmallDesktop() ? 'Open Source' : 'We\'re fully open source.' }</div>
+                    <div className={ styles.description }>Are you a developer?</div>
+                    <a className={ styles.buttonBox } href="https://github.com/ipfs-shipyard/pm-idm" target="_blank" rel="noopener noreferrer">
+                        <Button className={ styles.cta } variant="secondary" >
+                            <div className={ styles.buttonText }>CHECK OUR GITHUB <GithubIcon className={ styles.logo } /></div>
+                        </Button>
+                    </a>
+                </div>
+            </div>
+        );
+    }
+
+    updateDimensions() {
+        this.setState({ width: document.body.clientWidth });
+    }
+}
 
 CheckGithub.propTypes = {
     className: PropTypes.string,
--- src/shared/components/blocks/check-workshop/CheckWorkshop.js
+++ src/shared/components/blocks/check-workshop/CheckWorkshop.js
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './CheckWorkshop.module.css';
+import Button from '../../button';
 
 const CheckWorkshop = ({ className }) => (
     <div className={ classNames(styles.checkWorkshop, className) }>
-        This is the check workshop!
+        <div className={ styles.content }>
+            <div className={ styles.title }>Try it yourself</div>
+            <div className={ styles.description }>Follow a workshop to learn integrating within your DApp</div>
+            <a className={ styles.buttonBox } href="https://github.com/ipfs-shipyard/workshop-idm-chat-dapp" target="_blank" rel="noopener noreferrer">
+                <Button className={ styles.cta } variant="primary" >CHECK OUR WORKSHOP</Button>
+            </a>
+        </div>
     </div>
 );
 
