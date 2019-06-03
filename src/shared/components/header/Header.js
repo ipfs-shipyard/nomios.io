87b760bd950a5dcf8153ead82cdc3d1a28697eb9
Afonso Reis <antonio.reis@moxy.studio>
2019-06-03 15:43:37 +0100

    feat: add header

--- src/shared/components/header/Header.js
+++ src/shared/components/header/Header.js
@@ -2,11 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './Header.module.css';
+import Logo from '../logo';
+import Button from '../button';
+
+const anchorsObject = [
+    {
+        value: 'Benefits',
+        anchor: '',
+    },
+    {
+        value: 'Concept',
+        anchor: '',
+    },
+    {
+        value: 'Roadmap',
+        anchor: '',
+    },
+];
 
 const Header = ({ className }) => (
-    <div className={ classNames(styles.header, className) }>
-        This is the header!
-    </div>
+    <header className={ classNames(styles.header, className) } >
+        <div className={ (styles.headerContent) }>
+            <div className={ styles.anchorsContainer }>
+                {anchorsObject.map((item) => <a href={ `#${item.anchor}` } key={ item.value }> {item.value} </a>)}
+            </div>
+            <Logo className={ styles.headerLogo } variant={ 'horizontal' } />
+            <div className={ styles.buttonContainer } >
+                <Button variant={ 'secondary' }>
+                    <a href="#"> Join us </a>
+                </Button>
+            </div>
+        </div>
+    </header>
 );
 
 Header.propTypes = {
