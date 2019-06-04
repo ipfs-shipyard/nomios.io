58cde6f13dc8fe7ecbf922bf5cd964ba0ac6ef07
Afonso Reis <antonio.reis@moxy.studio>
2019-06-04 15:25:54 +0100

    feat: add hero

--- src/shared/components/blocks/hero/Hero.js
+++ src/shared/components/blocks/hero/Hero.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Svg from '../../svg';
+import lowpolySvg from '../../../media/illustrations/illustration-lowpoly-face.svg';
 import styles from './Hero.module.css';
 
 const Hero = ({ className }) => (
     <div className={ classNames(styles.hero, className) }>
-        This is the hero!
+        <div className={ styles.textWrapper } >
+            <h1>Open.</h1>
+            <h1>Decentralised.</h1>
+            <h1>Standard.</h1>
+        </div>
+        <Svg className={ styles.svgWrapper } svg={ lowpolySvg } />
+
+        <label className={ styles.vision } >
+        Vision
+            <span className={ styles.scrollHint } />
+        </label>
     </div>
 );
 
