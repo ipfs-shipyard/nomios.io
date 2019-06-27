34560aed44c366e935ebedd9f659810467a656a0
André Cruz <andremiguelcruz@msn.com>
2019-06-27 20:18:25 +0200

    chore: update roadmap

--- src/shared/components/blocks/roadmap/data.js
+++ src/shared/components/blocks/roadmap/data.js
@@ -1,5 +1,60 @@
 import { DoneIcon, DoingIcon, TodoIcon } from '../../icon';
 
+export const rows = [
+    {
+        name: 'Create identities based on DIDs',
+        status: [1, 0, 0],
+    },
+    {
+        name: 'Backup identity key material',
+        status: [1, 0, 0],
+    },
+    {
+        name: 'Manage multiple devices per identity',
+        status: [1, 0, 0],
+    },
+    {
+        name: 'Authenticate to applications',
+        status: [1, 0, 0],
+    },
+    {
+        name: 'Works directly within the browser, no extensions required',
+        status: [1, 0, 0],
+    },
+    {
+        name: 'Signing with session keys (derivates of device keys)',
+        status: [1, 0, 0],
+    },
+    {
+        name: 'Signing with device key (requires authorization)',
+        status: [1, 0, 0],
+    },
+    {
+        name: 'Support more DID methods',
+        status: [1, 0, 0],
+    },
+    {
+        name: 'Add for verifiable credentials, starting with social proofs',
+        status: [0, 1, 0],
+    },
+    {
+        name: 'Mobile based wallet',
+        status: [0, 1, 0],
+    },
+    {
+        name: 'Desktop based wallet',
+        status: [0, 1, 0],
+    },
+    {
+        name: 'Encryption of data between devices',
+        status: [0, 0, 1],
+    },
+    {
+        name: 'Support JSON-LD signatures',
+        status: [0, 0, 1],
+    },
+];
+
 export const status = [
     {
         name: 'Done',
@@ -12,44 +67,5 @@ export const status = [
     {
         name: 'To do',
         icon: TodoIcon,
-    },
-];
-
-export const rows = [
-    {
-        name: 'No plugins required',
-        status: [1, 0, 0],
-    },
-    {
-        name: 'Signing',
-        status: [1, 0, 0],
-    },
-    {
-        name: 'Mobile',
-        status: [1, 1, 0],
-    },
-    {
-        name: 'Authenticate',
-        status: [1, 1, 0],
-    },
-    {
-        name: 'Decentralized Identifiers (DIDs)',
-        status: [1, 0, 0],
-    },
-    {
-        name: 'Verifiable credentials',
-        status: [1, 0, 1],
-    },
-    {
-        name: 'Encryption between wallets',
-        status: [1, 0, 1],
-    },
-    {
-        name: 'Multi wallet',
-        status: [1, 0, 1],
-    },
-    {
-        name: 'Multi Identify',
-        status: [1, 0, 1],
     },
 ];
