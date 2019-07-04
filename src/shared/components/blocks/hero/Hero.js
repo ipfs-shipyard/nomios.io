6c49cf02463cf464d852adf1c3d3ee7ad8db5557
Gil Domingues <gil.domingues2@hotmail.com>
2019-07-04 11:51:57 +0100

    chore: first implementation with flat 3d rotation

--- src/shared/components/blocks/hero/Hero.js
+++ src/shared/components/blocks/hero/Hero.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Svg from '../../svg';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { throttle } from 'lodash';
+import Svg from '../../svg';
 import lowpolySvg from '../../../media/illustrations/illustration-lowpoly-face.svg';
 import styles from './Hero.module.css';
 
 const HEADER_OFFSET = 79;
+const MAX_ROTATION = 2;
 
 class Hero extends Component {
+    windowSize = null;
+    wrapperSize = null;
+    wrapperRef = React.createRef();
+
     state = {
         offset: typeof window !== 'undefined' && window.innerWidth <= 1024 ? 0 : HEADER_OFFSET,
+        rotX: 0,
+        rotY: 0,
     };
 
     componentDidMount() {
         if (typeof window !== 'undefined') {
             window.addEventListener('resize', this.updateWindowDimensions);
         }
+        this.updateWindowDimensions();
     }
 
     componentWillUnmount() {
@@ -24,15 +33,24 @@ class Hero extends Component {
     }
 
     render() {
-        const { offset } = this.state;
+        const { offset, rotX, rotY } = this.state;
         const { className } = this.props;
 
+        const transformation = `rotate3d(1,0,0,${rotY}deg) rotate3d(0,1,0,${rotX}deg)`;
+
         return (
-            <div className={ classNames(styles.hero, className) }>
+            <div
+                className={ classNames(styles.hero, className) }
+                onMouseMove={ this.handleMouseMove }
+                ref={ this.wrapperRef }>
                 <div className={ styles.textWrapper } >
                     <h1>Open.<br />Decentralized.<br />Standard.</h1>
                 </div>
-                <Svg className={ styles.svgWrapper } svg={ lowpolySvg } />
+                <div className={ styles.threeDWrapper }>
+                    <div className={ styles.svgWrapper } style={ { transform: transformation } }>
+                        <Svg svg={ lowpolySvg } />
+                    </div>
+                </div>
 
                 <AnchorLink offset={ offset } href="#why">
                     <div className={ styles.visionBox }>
@@ -53,7 +71,34 @@ class Hero extends Component {
 
         offset === HEADER_OFFSET && window.innerWidth <= 1024 && this.setState({ offset: 0 });
         offset === 0 && window.innerWidth > 1024 && this.setState({ offset: HEADER_OFFSET });
+
+        this.windowSize = { width: window.innerWidth, height: window.innerHeight };
+        this.wrapperSize = {
+            width: this.wrapperRef.current.offsetWidth,
+            height: this.wrapperRef.current.offsetHeight,
+            halfWidth: this.wrapperRef.current.offsetWidth / 2,
+            halfHeight: this.wrapperRef.current.offsetHeight / 2,
+        };
     };
+
+    updateHeadTransformation(mouseX, mouseY) {
+        const toCenterX = mouseX - this.wrapperSize.halfWidth;
+        const toCenterY = mouseY - this.wrapperSize.halfHeight;
+
+        const rotPercentageX = toCenterX / this.wrapperSize.halfWidth;
+        const rotPercentageY = 0 - (toCenterY / this.wrapperSize.halfHeight);
+
+        this.setState({
+            rotX: rotPercentageX * MAX_ROTATION,
+            rotY: rotPercentageY * MAX_ROTATION,
+        });
+    }
+
+    handleMouseMove = (event) => {
+        this.updateHeadTransformation(event.clientX, event.clientY);
+    };
+
+    handleResize = throttle(() => this.updateDimensions(), 250);
 }
 
 Hero.propTypes = {
